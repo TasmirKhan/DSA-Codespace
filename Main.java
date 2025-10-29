74a77750c68082f3f74a327241a8f90ea0a2f9ec
Tasmir Khan <masteroffcodes@gmail.com>
2025-10-29 10:54:49 -0700

    Add Main class with sum calculation example

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DSA Codespace 🚀");
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int num : arr) sum += num;
+        System.out.println("Sum = " + sum);
+    }
+}
